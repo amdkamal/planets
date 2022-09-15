c2e36ac06acc1d060e845502c1c6454403d08eea
amdkamal <amdkamal@gmail.com>
2022-09-15 20:33:39 +0400

    save and fetch films from/to localstorage

--- src/App.vue
+++ src/App.vue
@@ -20,53 +20,57 @@ export default {
   },
   methods: {
     async oneFunctionToGetThemAll() {
-      //function to fetch all results through pages
-      const APIRequest = async (url, data) => {
-        data = data || [];
-        await axios.get(url).then((res) => {
-          data.push(...res.data.results);
-          if (res.data.next !== null) {
-            return APIRequest(res.data.next, data);
-          }
+      try {
+        //function to fetch all results through pages
+        const APIRequest = async (url, data) => {
+          data = data || [];
+          await axios.get(url).then((res) => {
+            data.push(...res.data.results);
+            if (res.data.next !== null) {
+              return APIRequest(res.data.next, data);
+            }
+          });
+          return data;
+        };
+
+        //all planets
+        const allPlanets = await APIRequest("https://swapi.dev/api/planets");
+        //planets that have at least 2 film
+        const planetsInFilms = allPlanets.filter(
+          (planet) => planet.films.length > 1
+        );
+
+        let peopleInFilms = [];
+        planetsInFilms.forEach((planet) => {
+          peopleInFilms.push(...planet.residents);
         });
-        return data;
-      };
 
-      //all planets
-      const allPlanets = await APIRequest("https://swapi.dev/api/planets");
-      //planets that have at least 2 film
-      const planetsInFilms = allPlanets.filter(
-        (planet) => planet.films.length > 1
-      );
+        const people = await Promise.all(
+          peopleInFilms.map((person) => axios.get(person))
+        ).then((values) => {
+          return values.map((value) => value.data);
+        });
 
-      let peopleInFilms = [];
-      planetsInFilms.forEach((planet) => {
-        peopleInFilms.push(...planet.residents);
-      });
+        //planets that appear in at least 2 movies and have residents with min total of 5 starships
+        let planets = planetsInFilms;
+        planets.forEach((planet) => {
+          let newResidentList = people.filter((person) =>
+            planet.residents.includes(person.url)
+          );
+          planet.residents = newResidentList;
+        });
+        planets = planets.filter((planet) => {
+          let totalStarships = 0;
+          planet.residents.forEach(
+            (resident) => (totalStarships += resident.starships.length)
+          );
 
-      const people = await Promise.all(
-        peopleInFilms.map((person) => axios.get(person))
-      ).then((values) => {
-        return values.map((value) => value.data);
-      });
-
-      //planets that appear in at least 2 movies and have residents with min total of 5 starships
-      let planets = planetsInFilms;
-      planets.forEach((planet) => {
-        let newResidentList = people.filter((person) =>
-          planet.residents.includes(person.url)
-        );
-        planet.residents = newResidentList;
-      });
-      planets = planets.filter((planet) => {
-        let totalStarships = 0;
-        planet.residents.forEach(
-          (resident) => (totalStarships += resident.starships.length)
-        );
-
-        return totalStarships >= 5;
-      });
-      this.planets = planets;
+          return totalStarships >= 5;
+        });
+        this.planets = planets;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
   mounted() {
--- src/components/PlanetCard.vue
+++ src/components/PlanetCard.vue
@@ -8,22 +8,33 @@
         {{ this.timestamp(planet.created, "ddd MM, A") }}
       </div>
     </div>
-    <div class="card-body capitalize flex justify-between items-center">
-      <div class="flex items-center flex-row flex-wrap sm:flex-nowrap">
+    <div class="card-body capitalize flex md:justify-between items-center">
+      <div
+        class="flex items-center sm:flex-nowrap flex-row-reverse md:flex-row justify-between"
+      >
         <div
-          class="card-image mr-6 overflow-hidden rounded-3xl md:w-20 sm:w-1/6 w-20 mb-4"
+          class="card-image mr-6 overflow-hidden rounded-3xl md:w-20 sm:w-1/6 w-1/5 mb-4"
         >
           <img src="/icon.png" class="w-full" alt="" />
         </div>
-        <div>
-          <div class="font-bold font-white text-3xl mb-2">
-            {{ planet.name }}
+        <div class="flex items-center">
+          <div
+            class="block md:hidden text-3xl font-white font-bold bg-grey w-20 h-20 mr-4 rounded-full flex justify-center items-center"
+          >
+            <p>{{ planet.name.charAt(0) }}</p>
           </div>
-          <div class="font-bold font-grey text-2xl mb-2 md:hidden">
-            {{ planet.climate }}
-          </div>
-          <div class="font-grey text-xl">
-            Appears in {{ planet.films.length }} films
+          <div>
+            <div class="font-bold font-white text-3xl mb-2">
+              {{ planet.name }}
+            </div>
+            <div
+              class="font-regular md:font-bold font-grey text-2xl mb-2 md:hidden"
+            >
+              {{ planet.climate }}
+            </div>
+            <div class="planet-films text-xl">
+              Appears in {{ planet.films.length }} films
+            </div>
           </div>
         </div>
       </div>
@@ -38,14 +49,13 @@
     <div :class="`card-expand ${isExpanded ? '' : 'hidden'}`">
       <div class="wrapper mt-4">
         <Loading v-if="isLoading" />
-        <div v-else>
-          <div
-            v-for="film in planet.films"
-            :key="film.url"
-            class="font-grey text-xl font-bold"
-          >
-            {{ film.title }}
-          </div>
+        <div
+          v-else
+          v-for="film in planet.films"
+          :key="film.url"
+          class="font-grey text-xl font-bold"
+        >
+          {{ film.title }}
         </div>
       </div>
     </div>
@@ -64,6 +74,7 @@ export default {
       isExpanded: false,
       isLoading: false,
       filmsList: [],
+      filmsInStorage: null,
     };
   },
   methods: {
@@ -75,21 +86,59 @@ export default {
         this.isExpanded = false;
       } else {
         this.isExpanded = true;
+        this.isLoading = true;
         try {
-          this.isLoading = true;
-          const response = await Promise.all(
-            this.filmsList.map((film) => axios.get(film))
-          ).then((values) => values.map((value) => value.data));
-          this.planet.films = response;
+          const requestArr = this.filmsList.map(async (film) => {
+            return axios.get(film);
+          });
+          if (!this.filmsInStorage.length) {
+            //get films and populate storage
+            const returnedFilms = await Promise.all(requestArr).then(
+              (values) => {
+                return values.map((value) => value.data);
+              }
+            );
+            this.planet.films = returnedFilms;
+            this.filmsInStorage = returnedFilms;
+          } else {
+            let existingFilms = this.filmsInStorage.filter((film) => {
+              return this.filmsList.includes(film.url);
+            });
+            let nonExistingFilms = this.filmsInStorage.filter((film) => {
+              return !this.filmsList.includes(film.url);
+            });
+
+            if (!nonExistingFilms.length) {
+              //get films from storage if they all exist
+              this.planet.films = existingFilms;
+            } else {
+              //only fetch non existing films then add them to storage
+              let newRequests = nonExistingFilms.map((film) =>
+                axios.get(film.url)
+              );
+              let newData = await Promise.all(newRequests).then((values) => {
+                return values.map((value) => value.data);
+              });
+              this.planet.films = [newData, existingFilms];
+              this.filmsInStorage.push(...newData);
+            }
+          }
         } catch (error) {
           console.log(error);
         }
+        this.isLoading = false;
       }
-      this.isLoading = false;
     },
   },
   mounted() {
     this.filmsList = this.planet.films;
+    this.filmsInStorage = JSON.parse(localStorage.getItem("films")) || [];
+  },
+  watch: {
+    filmsInStorage(newValue, oldValue) {
+      //updates local storage when state changes
+      localStorage.setItem("films", JSON.stringify(newValue));
+    },
   },
 };
 </script>
